a0858ab259a4f0d8f47d767396db36cb94a187ed
Lucas Goldner <lucas.goldner@googlemail.com>
2020-12-09 12:41:18 +0100

    Websocket Server

--- src/main/java/space/nebulark/junisockets/Main.java
+++
@@ -1,8 +0,0 @@
-package space.nebulark.junisockets;
-
-public class Main {
-    public static void main(String[] args) {
-        // Ausgabe Hello World!
-        System.out.println("Hello World!");
-    }
-}
