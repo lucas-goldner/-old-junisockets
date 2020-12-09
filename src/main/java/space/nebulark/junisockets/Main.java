fccc40643490bf0aee2b58d1d081b16106fba572
Lucas Goldner <lucas.goldner@googlemail.com>
2020-12-09 12:31:16 +0100

    Managed to run the project

---
+++ src/main/java/space/nebulark/junisockets/Main.java
@@ -0,0 +1,8 @@
+package space.nebulark.junisockets;
+
+public class Main {
+    public static void main(String[] args) {
+        // Ausgabe Hello World!
+        System.out.println("Hello World!");
+    }
+}
